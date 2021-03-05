b6372caf57ec51449693e5f2a15fd4bda0f622d3
dltmdwlr <dltmdwlr@gmail.com>
2021-03-05 10:26:47 +0900

    replace

--- AI-1.sql
+++ AI-1.sql
@@ -17,3 +17,32 @@ SELECT INSTR('이것이 Oracle이다. 이것도 오라클이다', '이것', 2) FROM DUAL;
 
 --(문자열이 시작하는 위치를 BYTE단위로 세서 반환)
 SELECT INSTRB('이것이 Oracle이다. 이것도 오라클이다', '이것', 2) FROM DUAL;
+
+
+
+--Upper, Lower, Initcap
+--
+--(UPPER 모두 대문자, LOWER 모두 소문자, INITCATP 첫 글자를 모두 대문자)
+SELECT LOWER('abcdEFGH'), UPPER('abcdEFGH'), INITCAP('this is oracle') FROM DUAL;
+
+SELECT e.ename, e.sal from emp e where lower(e.ename) = lower('ward');
+
+
+
+--Replace, Translate, Substr, Reverse
+--
+--(REPLACE 문자열을 바꿔준다)
+SELECT REPLACE ('이것이 Oracle이다', '이것이' , 'This is') FROM DUAL;
+
+--(TRANSLATE 문자열을 한 글자씩 바꿔준다)
+SELECT TRANSLATE('이것이 Oracle이다', '이것' , 'AB') FROM DUAL;
+
+SELECT TRANSLATE('이것이 OracleOradddleclt이다', 'Orac', '123') FROM DUAL;
+
+--(SUBSTR(문자열, 시작위치, 길이) 시작위치부터 길이만큼 문자반환)
+SELECT SUBSTR('대한민국만세', 3, 2) FROM DUAL;
+
+SELECT (SUBSTR('삼성전자와 애플의 전자대결.', 1, 4) || 'VS' || SUBSTR('삼성전자와 애플의 전자대결.', 7, 2)) FROM DUAL;
+
+--(REVERSE 문자열의 순서를 거꾸로 만든다)
+SELECT REVERSE('Oracle') FROM DUAL;

3ffab5adb7d706775f3fd70cd115bcef42eadcd8
dltmdwlr <dltmdwlr@gmail.com>
2021-03-10 09:59:52 +0900

    Update AI-2.sql

--- AI-2.sql
+++ AI-2.sql
@@ -15,6 +15,37 @@ alter table Ptable add constraint fkfk foreign key (Column3) references fktable 
 
 
 
+--Unique
+--
+--(데이터의 중복 허용X , 빈 값(Null 값) 허용)
+create table UniqueEx(colunm1 varchar2(20) constraint column1Unique Unique, column2 number(3) constraint colum2Unique Unique, column3 char(3));
+
+insert into uniqueex values ('1', 2, '');
+insert into uniqueex values ('2', 2, '');
+insert into uniqueex values ('3', 4, '');
+
+
+
+--Check
+--
+--(특정 범위 값만 지정, 특정 값만 지정)
+create table checkex(column1 number(3) constraint column1check check(column1 between 1 and 10), column2 varchar2(20) constraint column2check check(column2 In('Y', 'N')), column3 varchar2(10));
+
+insert into checkex values (2, 'Y', '');
+insert into checkex values (20, 'PPP', '');
+
+--(이미 생성된 테이블에 CHECK 지정할 때)
+--(ALTER TABLE 테이블명 ADD CONSTRAINT 제약조건명 CHECK (컬럼명 및 조건))
+
+alter table checkex add constraint chk3 check (column3 in ('A','B','C','D','F'));
+
+
+
+
+
+
+
+
 
 
 

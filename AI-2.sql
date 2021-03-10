07ca415bfdb34b782d04740f70bceead182e5c89
dltmdwlr <dltmdwlr@gmail.com>
2021-03-10 10:48:36 +0900

    Update AI-2.sql

--- AI-2.sql
+++ AI-2.sql
@@ -1,4 +1,3 @@
-
 --Foreign Key
 --
 --(이미 생성된 테이블간 FOREIGN KEY 지정할 때)
@@ -39,27 +38,35 @@ insert into checkex values (20, 'PPP', '');
 
 alter table checkex add constraint chk3 check (column3 in ('A','B','C','D','F'));
 
+insert into checkex values (1, 'N', 'A');
+insert into checkex values (1, 'N', 'K');
+insert into checkex values (1, 'N', 'A');
 
+--(기본값을 지정한다)
+create table defaultex(column1 varchar(30), column2 varchar2(20) default '기본값' , column3 number(8) Default 1234 );
 
+insert into defaultex (column1) values ('땡땡');
+--insert into defaultex values ('땅땅', default, default);
+insert into defaultex values ('당당', null, null);
 
+--(이미 생성된 테이블에 DEFAULT 지정할 때)
+--(ALTER TABLE 테이블명 MODIFY 컬럼명 DEFAULT 값지정;)
+alter table defaultex modify column3 default 123;
 
+--(이미 생성된 테이블에 NOT NULL 지정)
+--(ALTER TABLE 테이블명 MODIFY 컬럼명 NOT NULL;)
+create table NotNullEx(column1 varchar2(20) constraint column1NotNull not null , column2 number(3) constraint column2NotNull not null);
 
+alter table notnullex modify column2 constraint column2NotNull not null;
 
+insert into notnullex values ('101', '222');
+insert into notnullex values ('102', Null);
+insert into notnullex values ('103', '333');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--제약 조건 삭제
+--(이미 존재하는 COLUMN의 제약 조건 삭제 시)
+--(ALTER TABLE [테이블명] DROP CONSTRAINT CONSTRAINT_NAME;)
+alter table notnullex drop constraint column2notnull;
 
 
 

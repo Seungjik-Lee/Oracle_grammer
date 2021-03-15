5eebc33f4b946ba1b3783aae7c9b88ab5aa39f2b
kimduhee7524 <dltmdwlr@gmail.com>
2021-03-15 15:55:30 +0900

    커밋

---
+++ AI1.sql
@@ -0,0 +1,28 @@
+-- DDL CREATE, ALTER, DROP 테이블 생성, 수정, 삭제
+CREATE TABLE atable(idx int, name varchar2(30), gender varchar2(10), remark varchar2(200) );
+
+--DML INSERT
+INSERT INTO "AI"."ATABLE"(IDX, NAME, GENDER, REMARK) VALUES ('1','홍길동','남자','비고');
+
+select * from atable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
